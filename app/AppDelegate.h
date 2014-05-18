af6f817b65d90fbf528c83e5804d914ac057062b
Yan Li <eyeplum@gmail.com>
2014-05-18 16:23:39 +0800

    Initial commit

---
+++ app/AppDelegate.h
@@ -0,0 +1,8 @@
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (nonatomic, strong) IBOutlet NSWindow *window;
+@property (nonatomic, strong) IBOutlet NSWindow *secondWindow;
+
+@end
---
+++ app/AppDelegate.m
@@ -0,0 +1,11 @@
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+  [self.window makeKeyAndOrderFront:nil];
+  [self.secondWindow makeKeyAndOrderFront:nil];
+}
+
+@end
+
